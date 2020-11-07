b96449a6aeddbfd1f348eb6d5f3ac97a1107a1fd
maribib <maribib.cerkinfo@gmail.com>
2020-11-07 20:05:23 +0100

    [add] logos [ref] assets [add] banner

--- src/Root.js
+++ src/Root.js
@@ -4,67 +4,67 @@ import useDonations from './DonationContext';
 
 const streams = [
   {
-    icon: process.env.PUBLIC_URL+"/ace.png",
+    icon: process.env.PUBLIC_URL+"/logos/ace.png",
     title: "ACE",
     id: "twitch-embed-ace",
     channel: "ace-ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/orga.png",
     title: "Team Orga",
     id: "twitch-embed-orga",
     channel: "team-organisateur"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/ci.png",
     title: "CI",
     id: "twitch-embed-CI",
     channel: "ci_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/cepha.png",
     title: "CePha",
     id: "twitch-embed-CEPHA",
     channel: "cepha_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/cgeo.png",
     title: "CGéo",
     id: "twitch-embed-CGEO",
     channel: "cgeo_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/polesante.png",
     title: "ISEP-CM-CKO-CO",
     id: "twitch-embed-sante",
     channel: "polesante_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/cig.png",
     title: "CIG",
     id: "twitch-embed-CIG",
     channel: "cig_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/orga.png",
     title: "Régionales",
     id: "twitch-embed-inter",
     channel: "inter_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/philo_psy.png",
     title: "CPL-CdH-CJC-CROM-CHAA-CPSY",
     id: "twitch-embed-solb1",
     channel: "celb_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/cp_cds.png",
     title: "CdS-CP",
     id: "twitch-embed-Cds-CP",
     channel: "cp_cds_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/care.png",
     title: "CARé",
     id: "twitch-embed-CARé",
     channel: "care_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/cd_isti.png",
     title: "CD-ISTI",
     id: "twitch-embed-CD-ISTI",
     channel: "isti_ulb"
   }, {
-    icon: process.env.PUBLIC_URL+"/orga.png",
+    icon: process.env.PUBLIC_URL+"/logos/agro.png",
     title: "AGRO",
     id: "twitch-embed-AGRO",
     channel: "agro_ulb"
@@ -133,18 +133,21 @@ const Root = () => {
   }
 
   return (
-    <div className="container" style={{padding:"0px 1em"}}>
-      <div className="row">
-        <div className="custom-control custom-switch text-light">
-          <input type="checkbox" className="custom-control-input" id="customSwitch1" checked={showStream} onChange={e => setShowStream(e.target.checked)} />
-          <label className="custom-control-label" htmlFor="customSwitch1">Display twitch streams <span>(warning it may cause performance issues)</span></label>
+    <div>
+      <div className="text-center banner" />
+      <div className="container" style={{padding:"0px 1em"}}>
+        <div className="row">
+          <div className="custom-control custom-switch text-light">
+            <input type="checkbox" className="custom-control-input" id="customSwitch1" checked={showStream} onChange={e => setShowStream(e.target.checked)} />
+            <label className="custom-control-label" htmlFor="customSwitch1">Display twitch streams <span>(warning it may cause performance issues)</span></label>
+          </div>
         </div>
+        <div className="row">
+          { streams.map(props => (
+            <Stream {...props} key={props.id} showStream={showStream} donations={getPoolDonation(props.channel)} />
+          ))}
+    		</div>
       </div>
-      <div className="row">
-        { streams.map(props => (
-          <Stream {...props} key={props.id} showStream={showStream} donations={getPoolDonation(props.channel)} />
-        ))}
-  		</div>
     </div>
   );
 }
