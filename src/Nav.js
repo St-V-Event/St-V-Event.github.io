d05d65af26fdc65ca6cf7c72893dc6eeddec421d
maribib <maribib.cerkinfo@gmail.com>
2020-11-07 12:47:53 +0100

    [fix] logo url

--- src/Nav.js
+++ src/Nav.js
@@ -7,9 +7,9 @@ const Nav = () => {
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"/>
       </button>
-      <a className="navbar-brand" href="#">
+      <Link className="navbar-brand" to="/">
         <img src="/logo192.png" alt="" />
-      </a>
+      </Link>
 
       <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
