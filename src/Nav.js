f366f61b28d2f9006ffe73a797352e48f7d1d405
maribib <maribib.cerkinfo@gmail.com>
2020-11-08 17:46:59 +0100

    [fix] donate button font weight

--- src/Nav.js
+++ src/Nav.js
@@ -31,7 +31,7 @@ const Nav = () => {
         </ul>
         <ul className="navbar-nav navbar-right">
           <li className="nav-item">
-            <Link to="/donate/team-organisateur" className=" btn text-dark btn-warning">
+            <Link to="/donate/team-organisateur" className="btn text-dark btn-warning font-weight-bold">
               Donate!
             </Link>
           </li>
