ef67d4303137dec955114313d573addacd036571
maribib <maribib.cerkinfo@gmail.com>
2020-11-08 16:00:14 +0100

    [add] association description

--- src/Association.js
+++ src/Association.js
@@ -1,7 +1,11 @@
 const Association = () => {
   return (
-    <div>
-
+    <div className="container text-light">
+      <h2>Centre de prévention des violences conjugales et familiales <a href="http://www.cpvcf.org/" target="_blank"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-link" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/>
+  <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"/>
+</svg></a></h2>
+      Le CPVCF propose un accompagnement spécialisé à toute personne concernée par les violences conjugales et/ou intrafamiliales (femmes majoritairement) venant de tous les milieux sociaux, culturels, professionnels, religieux, ethniques et philosophiques. Cet accompagnement lui permet de (re)trouver l’estime de soi et de (re)prendre du pouvoir sur sa vie, il s’exerce à travers plusieurs choses (une écoute téléphonique, un accueil, une aide administrative et sociale, un espace de parole en individuel (pour femme, homme, auteur ou victime) ou en couple, des ateliers d’Education Permanente un hébergement pour femmes (avec ou sans enfant(s) sans limite d’âge) à une adresse confidentielle). Le Centre s’adresse également aux institutions, associations, services qui sont amenés à intervenir auprès des personnes concernées par la problématique des violences conjugales et intrafamiliales. Il est fréquemment consulté pour son expertise en cette matière (relais auprès des victimes, information, formation des intervenants, des associations, services, institutions,…). Depuis sa création, il a été et reste un interlocuteur actif auprès des pouvoirs publics en matière de violence conjugale.
     </div>
   );
 }
