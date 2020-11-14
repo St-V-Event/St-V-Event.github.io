f0d4669d685fa0a072f8a68eac8edb1b9fb4824c
maribib <maribib.cerkinfo@gmail.com>
2020-11-14 18:53:05 +0100

    [fix] missing file

---
+++ src/Discord.js
@@ -0,0 +1,8 @@
+
+const Discord = () => {
+  return (
+    <iframe className="discord" title="Discord" src="https://discordapp.com/widget?id=774659458139553852&theme=dark" width="350" height="500" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+  )
+}
+
+export default Discord;
