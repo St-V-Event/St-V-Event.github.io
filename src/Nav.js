831e44c9d0dd101fcc4075a472f6dc6765619439
maribib <maribib.cerkinfo@gmail.com>
2020-11-14 18:52:36 +0100

    [fix] nav active

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import useDonations from './DonationContext';
 
 const Nav = () => {
   let {donations} = useDonations();
   const totalDonations = () => Object.values(donations).reduce((acc,val) => acc + val, 0);
+
+  let location = useLocation();
+
+  const navItemClass = url => {
+    return "nav-item"+(url===location.pathname && " active")
+  }
+
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,16 +23,16 @@ const Nav = () => {
 
       <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+          <li className={navItemClass("/")}>
+            <Link className="nav-link" to="/">Home</Link>
           </li>
-          <li className="nav-item">
+          <li className={navItemClass("/association")}>
             <Link className="nav-link" to="/association">Association</Link>
           </li>
-          <li className="nav-item">
+          <li className={navItemClass("/schedule")}>
             <Link className="nav-link" to="/schedule">Schedule</Link>
           </li>
-          <li className="nav-item">
+          <li className={navItemClass("/shop")}>
             <Link className="nav-link" to="/shop">Shop</Link>
           </li>
         </ul>
