6d05d8e6e6d4cedabed480177368ed9a9ff1cacf
maribib <maribib.cerkinfo@gmail.com>
2021-11-14 18:51:19 +0100

    [fix]

--- src/Sablon.js
+++ src/Sablon.js
@@ -4,15 +4,15 @@ const Sablon = () => {
   return (
     <div className="container text-center">
       <br/>
-      <img src={process.env.PUBLIC_URL+"sablon_1.png"} />
+      <img src={process.env.PUBLIC_URL+"/sablon/sablon_1.png"} />
       <br/><br/>
-      <img src={process.env.PUBLIC_URL+"sablon_2.png"} />
+      <img src={process.env.PUBLIC_URL+"/sablon/sablon_2.png"} />
       <br/><br/>
-      <img src={process.env.PUBLIC_URL+"sablon_3.png"} />
+      <img src={process.env.PUBLIC_URL+"/sablon/sablon_3.png"} />
       <br/><br/>
-      <img src={process.env.PUBLIC_URL+"sablon_4.png"} />
+      <img src={process.env.PUBLIC_URL+"/sablon/sablon_4.png"} />
       <br/><br/>
-      <img src={process.env.PUBLIC_URL+"sablon_5.png"} />
+      <img src={process.env.PUBLIC_URL+"/sablon/sablon_5.png"} />
       <br/><br/>
     </div>
   );
